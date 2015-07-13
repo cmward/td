2f389803d2b9bc492d5522d1594032b28efc1ec5
Chris Ward <cmward@brandeis.edu>
2015-07-13 13:56:12 -0400

    Tasks are now objects.

--- README.md
+++ README.md
@@ -9,10 +9,11 @@ That should be it.
 ### Usage
 `td` will list the tasks in the list in your working directory if it finds a td file, and if not, it will create such a file.  
 ```
--c ID      completes a task, moving it to the completed list  
--f         lists completed tasks for the current directory  
--e ID TASK edits the task with ID to be TASK  
--w         wipes the history of completed tasks in the current directory
+-c ID           completes a task, moving it to the completed list  
+-f              lists completed tasks for the current directory  
+-e ID TASK      edits the task with ID to be TASK  
+-u USERNAME     add a username stamp to the task
+-w              wipes the history of completed tasks in the current directory
 ```
 
 Example usage:  
@@ -26,4 +27,4 @@ $ td
 0 Go to the store.
 ```
 
-The lists are stored as `.td.txt`.
+The lists are stored as `td.txt`.
--- td.py
+++ td.py
@@ -1,119 +1,154 @@
 #!/usr/bin/env python
+"""Less elegant, less functional, and all around stupider version of
+Steve Losh's t (http://stevelosh.com/projects/t/).
+
+Create independent todo lists for individual directories.
+"""
 import argparse
 import os
 import time
 
-"""Less elegant, less functional, and all around stupider version of
-Steve Losh's t (http://stevelosh.com/projects/t/).
-
-Create independent todo lists for individual directories. 
-"""
-
 def _parse_time():
+    """Return the current time, nicely formatted."""
     return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
-class Tasks(object):
-    """ Maps ids to tasks. Ids are created for each item in the list
-    sequentially. 
-    
+def _parse_task_from_file(line):
+    """Read task from td.txt file and create the corresponding
+    task object.
+    """
+    time = line[1:20]
+    line = line[22:]
+    completed = False
+    if line.endswith("*done*"):
+        completed = True
+        line = line[:line.find("*done*")-1]
+    if line.find("*un*") != -1:
+        # +5 skips over '*un* '
+        username = line[line.find("*un*")+5:]
+        line = line[:line.find("*un*")]
+    else:
+        username = None
+    text = line
+    task = Task(text, time, username, completed)
+    return task
+
+class Task(object):
+    """Structure representing a single task. Stores the task string,
+    the time it was created, the username of its creator (if it has one),
+    and a flag indicating whether or not it's completed.
+    """
+    def __init__(self, text, time, username=None, completed=False):
+        """Each task object will have to have all of these parameters
+        supplied at time of creation.
+        """
+        self.text = text
+        self.time = time
+        self.username = username
+        self.completed = completed
+
+    def __str__(self):
+        if self.username:
+            return "[" + self.time + "]" + " " + self.text + " " + \
+                    "(" + self.username + ")"
+        else:
+            return "[" + self.time + "]" + " " + self.text
+
+class TaskDict(object):
+    """Maps ids to tasks. Ids are created for each item in the list
+    sequentially.
+
     Read in from the task text file on init and write out with write().
     Creates td.txt in dir if it doesn't already exist.
     """
-    def __init__(self, dir):
+    def __init__(self, directory):
         self.tasks = {}
         self.completed_tasks = {}
-        self.dir = os.path.abspath(dir)
-        self.file = os.path.join(self.dir, ".td.txt")
+        self.directory = os.path.abspath(directory)
+        self.file = os.path.join(self.directory, "td.txt")
         try:
-            with open(self.file, 'r') as f:
+            with open(self.file, 'r') as task_file:
                 i, j = 0, 0
-                for line in f:
+                for line in task_file:
                     clean_line = line.strip()
                     if clean_line != "":
-                        if clean_line.endswith("*done*"):
-                            self.completed_tasks[j] = (clean_line[:19],
-                                                       clean_line[20:])
-                            j += 1
-                        else:
-                            self.tasks[i] = (clean_line[:19],
-                                             clean_line[20:])
+                        task = _parse_task_from_file(clean_line)
+                        if task.completed:
+                            self.completed_tasks[i] = task
                             i += 1
+                        else:
+                            self.tasks[j] = task
+                            j += 1
         except IOError:
-            print "Couldn't find .td.txt. \n Creating it in %s." \
-                    % os.path.abspath(dir)
+            print "Couldn't find td.txt. Creating it in %s." \
+                    % os.path.abspath(directory)
             self.write()
 
     def _next_id(self, dict_name):
-        """ Return the next available id.
-        """
+        """Return the next available id."""
         return len(self.__dict__[dict_name])
 
-    def _update_ids(self, n, dict_name):
-        """ Subtract 1 from all ids greater than n in the specified dict.
+    def _update_ids(self, delete_n, dict_name):
+        """Subtract 1 from all ids greater than n in the specified dict.
         """
-        d = self.__dict__[dict_name]
-        for k in d.iterkeys():
-            if k > n:
-                d[k-1] = d.pop(k)
+        dictionary = self.__dict__[dict_name]
+        for key in dictionary.iterkeys():
+            if key > delete_n:
+                dictionary[key-1] = dictionary.pop(key)
 
-    def add_task(self, task):
-        """ Add a task to the task dictionary.
-        """
-        self.tasks[self._next_id('tasks')] = (_parse_time(), task)
+    def add_task(self, text, username=None):
+        """Add a task to the task dictionary."""
+        new_task = Task(text, _parse_time(), username, completed=False)
+        self.tasks[self._next_id('tasks')] = new_task
 
     def complete_task(self, task_id):
         """ Remove the task specified by the given id from the tasks dict
         and add it to the completed_tasks dict.
         """
-        task = self.tasks[task_id][1] + " *done*"
+        task = self.tasks[task_id]
+        text = task.text + " *done*"
+        username = task.username
         self.completed_tasks[self._next_id('completed_tasks')] = \
-                (_parse_time(), task)
+                Task(text, _parse_time(), username, completed=True) 
         del self.tasks[task_id]
         self._update_ids(task_id, 'tasks')
 
     def edit_task(self, new_task, task_id):
-        self.tasks[task_id] = (self.tasks[task_id][0], new_task)
+        """Change a task in the dictionary."""
+        self.tasks[task_id].text = new_task
 
     def wipe_completed_history(self):
-        """ Wipe the completed tasks dictionary. Won't take effect in the
+        """Wipe the completed tasks dictionary. Won't take effect in the
         .txt file until write() is called.
         """
         self.completed_tasks = {}
         print "Completed task history wiped."
 
-    def print_task(self, id, dict_name):
-        if dict_name == "tasks":
-            print self.__dict__[dict_name][id][0], ">>", \
-                    id, "-", self.__dict__[dict_name][id][1] 
-        else:
-            print self.__dict__[dict_name][id][0], ">>", \
-                    id, "-", self.__dict__[dict_name][id][1][:-6]
-
     def print_dict(self, dict_name):
-        if len(self.__dict__[dict_name]) == 0:
+        """Print the entire dictionary specified by dict_name."""
+        task_dict = self.__dict__[dict_name]
+        if len(task_dict) == 0:
             print "No tasks in this list."
         else:
-            map(lambda k: self.print_task(k, dict_name), \
-                self.__dict__[dict_name].keys()) 
+            for task_id, task in task_dict.iteritems():
+                print task_id, ">>", task 
 
     def write(self):
-        """ Write the tasks and completed tasks out to a .txt file.
+        """Write the tasks and completed tasks out to a .txt file.
         Overwrites the existing file.
         """
         with open(self.file, 'w') as out:
             for task in self.tasks.values():
-                out.write(task[0]+' '+task[1]+'\n')
+                out.write(str(task) + '\n')
             out.write('\n')
-            for ct in self.completed_tasks.values():
-                out.write(ct[0]+' '+ct[1]+'\n')
+            for completed in self.completed_tasks.values():
+                out.write(str(completed) + " " + "*done*" + '\n')
 
 def _main():
-    """ Parse arguments and perform the proper actions.
-    """
+    """Parse arguments and perform the proper actions."""
     parser = argparse.ArgumentParser(prog="td")
-    parser.add_argument("--dir", dest="dir", 
+    parser.add_argument("--dir", dest="dir",
                         help="Work on tasks in DIR")
-    parser.add_argument("-c", "--complete", dest="complete", 
+    parser.add_argument("-c", "--complete", dest="complete",
                         required=False, help="Mark task as completed")
     parser.add_argument("-e", "--edit", dest="edit",
                         required=False, help="Edit TASK")
@@ -122,35 +157,40 @@ def _main():
                         help="Wipe completed task history")
     parser.add_argument("-f", "--finished", action="store_true",
                         help="Display list of finished tasks.")
+    parser.add_argument("-u", "--username", dest="username",
+                        required=False)
     parser.add_argument("text", nargs="*", metavar="TASK",
                         help="Add TASK to the list.")
-    args=parser.parse_args()
-    
-    t = Tasks(args.dir)
+    args = parser.parse_args()
+
+    tasks = TaskDict(args.dir)
     task = ' '.join(args.text)
     if args.complete:
-        t.complete_task(int(args.complete))
-        t.write()
+        tasks.complete_task(int(args.complete))
+        tasks.write()
     elif args.edit and args.text:
-        t.edit_task(task, int(args.edit))
-        t.write()
+        tasks.edit_task(task, int(args.edit))
+        tasks.write()
     elif args.wipe:
         print "Wipe completed task history? (y/n)"
-        input = raw_input()
-        if input == "y":
-            t.wipe_completed_history()
-            t.write()
+        user_input = raw_input()
+        if user_input == "y":
+            tasks.wipe_completed_history()
+            tasks.write()
         elif input == "n":
             quit()
         else:
             print "Please enter (y) or (n)"
     elif args.text:
-        t.add_task(task)
-        t.write()
+        if args.username:
+            tasks.add_task(task, username=args.username)
+        else:
+            tasks.add_task(task)
+        tasks.write()
     elif args.finished:
-        t.print_dict("completed_tasks")
+        tasks.print_dict("completed_tasks")
     else:
-        t.print_dict("tasks")
+        tasks.print_dict("tasks")
 
 if __name__ == "__main__":
     _main()
